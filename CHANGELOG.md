ba6dadd2a59ea9fc3c882d51f805133e0260330b
Lance Albertson <lance@osuosl.org>
2019-10-15 11:23:41 -0700

    Initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+networkmanager CHANGELOG
+========================
+This file is used to list changes made in each version of the
+networkmanager cookbook.
+
+0.1.0
+-----
+- Initial release of networkmanager
+
---
+++ metadata.rb
@@ -0,0 +1,12 @@
+name             'networkmanager'
+maintainer       'Oregon State University'
+maintainer_email 'chef@osuosl.org'
+license          'Apache-2.0'
+chef_version     '>= 13'
+issues_url       'https://github.com/osuosl-cookbooks/networkmanager/issues'
+source_url       'https://github.com/osuosl-cookbooks/networkmanager'
+description      'Installs/Configures networkmanager'
+long_description 'Installs/Configures networkmanager'
+version          '0.1.0'
+
+supports         'centos', '~> 7.0'
